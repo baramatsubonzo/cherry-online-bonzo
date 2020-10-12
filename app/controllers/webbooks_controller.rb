6d97bebe7ffe29c7a41f7df7484ba12e93ada03d
baramatsubonzo <baramatsubonzo@gmail.com>
2020-10-12 11:44:13 +0900

    新規投稿画面の作成

--- app/controllers/webbooks_controller.rb
+++ app/controllers/webbooks_controller.rb
@@ -6,9 +6,17 @@ class WebbooksController < ApplicationController
   end
 
   def new
+    @webbook = Webbook.new
   end
 
   def create
+    @webbook = Webbook.new(webbook_params)
+    if @webbook.save
+      flash[:success] = "新しいWEBブックを作成しました"
+      redirect_to root_url
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -18,5 +26,12 @@ class WebbooksController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def webbook_params
+    params.require(:webbook)
+          .permit(:title, :author, :description, :price, :release_date, :release)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  root "webbooks#index"
   resources :webbooks
 end
