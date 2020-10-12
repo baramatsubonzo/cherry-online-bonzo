7851710f918b8e43501c2da66709d56b0442296c
bonzo <40222661+baramatsubonzo@users.noreply.github.com>
2020-10-12 20:55:31 +0900

    Revert "Webbookの一覧、詳細、編集画面の実装"

--- app/models/webbook.rb
+++
@@ -1,3 +0,0 @@
-class Webbook < ApplicationRecord
-  has_one_attached :image
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
 Rails.application.routes.draw do
-  root "webbooks#index"
-  resources :webbooks
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
