0feb7597426870911fc88edb7d24e7ab17044cfd
baramatsubonzo <baramatsubonzo@gmail.com>
2020-10-15 14:47:18 +0900

    ログイン・ログアウト機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   root "webbooks#index"
   resources :webbooks
   resources :users, only: [:new]
+  namespace :users do
+    get 'login', to: 'session#new'
+    post 'login', to: 'session#create'
+    delete 'logout', to: 'session#destroy'
+  end
 end
