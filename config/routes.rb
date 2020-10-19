0cbfec414583d4a78db318e5b13fa951733bec05
baramatsubonzo <baramatsubonzo@gmail.com>
2020-10-19 16:23:21 +0900

    wip ログインができる

--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,8 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new'
   post 'signup', to: 'users#create'
 
-  # get 'login', to: 'session#new'
-  # post 'login', to: 'session#create'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
   # delete 'logout', to: 'session#destroy'
 
   # resources :charges
