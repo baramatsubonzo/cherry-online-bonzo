9722129c4805e130d832859dd1a8defd79029d4e
baramatsubonzo <baramatsubonzo@gmail.com>
2020-10-20 19:57:43 +0900

    add_purchase_historyメソッドの責務分離

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -10,26 +10,14 @@ class ChargesController < ApplicationController
     @stripe_form = StripeForm.new(params[:stripeEmail], params[:stripeToken], @webbook)
     @stripe_form.save!
 
-    # 購入が成功した場合の処理
-    # ここに購入完了のメール通知処理を実装する
-    # 履歴が作成されることを追加する
-    add_purchase_history
+    @purchase_history_webbooks = PurchaseHistoryWebbook.new
+    @purchase_history_webbooks.add_purchase_history(current_user, params[:purchase_history_id], @webbook.id)
 
     PurchaseMailer.creation_email(current_user).deliver_now
     redirect_to webbook_path(params[:id]), notice: "商品を購入しました！"
   end
 
   private
-
-  def add_purchase_history
-    if @purchase_history_webbooks.blank?
-      # RFC: この渡し方で本当にいいのか？
-      @purchase_history_webbook = current_user.purchase_history.purchase_history_webbooks.build(purchase_history_id: params[:purchase_history_id], webbook_id: @webbook.id)
-    end
-    @purchase_history_webbook.save
-    # todo: 後でわかりやすく変更する
-    # redirect_to current_cart
-  end
 
   def prevent_duouble_purchase
     redirect_to root_path, notice: "すでに購入済みの商品です" if user_has_book?
