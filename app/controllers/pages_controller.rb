1ac682959711d98836f28f301c2da00bf4869ecf
baramatsubonzo <baramatsubonzo@gmail.com>
2020-10-21 12:18:27 +0900

    新規pageを投稿できるようにした

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,8 +6,26 @@ class PagesController < ApplicationController
   end
 
   def new
+    @page = Page.new
+  end
+
+  def create
+    @page = Page.new(page_params.merge(webbook_id: params[:webbook_id]))
+    if @page.save
+      flash[:success] = "新しいページを作成しました"
+      redirect_to root_url
+    else
+      render 'new'
+    end
   end
 
   def edit
+  end
+
+  private
+
+  def page_params
+    params.require(:page)
+          .permit(:title, :content)
   end
 end
