767b25212bf4556a72558477ac11d20470fff8b1
baramatsubonzo <baramatsubonzo@gmail.com>
2020-10-21 11:11:35 +0900

    pageモデルの作成

--- app/models/webbook.rb
+++ app/models/webbook.rb
@@ -1,6 +1,8 @@
 class Webbook < ApplicationRecord
   has_one_attached :image
 
+  has_many :pages, dependent: :destroy
+
   has_many :cart_webbooks
   has_many :purchase_history_webbooks
 end
---
+++ spec/factories/pages.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :page do
+    title { "MyString" }
+    content { "MyText" }
+    webbook { nil }
+  end
+end
