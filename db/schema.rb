d07cfdf2f1197e60ff0bc48b6e46a03825e8d1d1
baramatsubonzo <baramatsubonzo@gmail.com>
2020-10-22 19:55:45 +0900

    Userモデルへ管理者フラグの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_21_020943) do
+ActiveRecord::Schema.define(version: 2020_10_22_104932) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 2020_10_21_020943) do
     t.bigint "webbook_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "row_order"
     t.index ["webbook_id"], name: "index_pages_on_webbook_id"
   end
 
@@ -82,6 +83,7 @@ ActiveRecord::Schema.define(version: 2020_10_21_020943) do
     t.string "password_digest", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
