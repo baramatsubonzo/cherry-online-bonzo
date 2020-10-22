e18e93c6e6091f81de278be20e43bbb8899af3e7
baramatsubonzo <baramatsubonzo@gmail.com>
2020-10-22 14:23:14 +0900

    マイページの殻を作成してみた

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   post '/add_webbook' => 'carts#add_webbook'
   delete '/delete_webbook' => 'carts#delete_webbook'
 
@@ -9,6 +10,13 @@ Rails.application.routes.draw do
   end
 
   resources :carts, only:[:show]
+
+  # RFC: 今後管理者がユーザー一覧、詳細画面を見る可能性があると考えた。
+  # そのため、ユーザーが自分の情報を見るためには、mypageというリソースから閲覧することにする。
+  # TODO: user一覧ページの作成
+  resources :users, only:[:index] do
+    resource :mypage, only:[:show]
+  end
 
   get 'signup', to: 'users#new'
   post 'signup', to: 'users#create'
