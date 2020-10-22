bc552df6aff3a033433b875ddf3322dd7b040bbd
baramatsubonzo <baramatsubonzo@gmail.com>
2020-10-22 16:26:38 +0900

    購入しているユーザーしかWebbookを閲覧できないようにした

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,8 +5,9 @@ class PagesController < ApplicationController
   end
 
   def show
-    @webbook = Webbook.find(params[:webbook_id])
     # FYI: 購入履歴になかったら、本を読めないように制限する
+    # binding.pry
+    @webbook = Webbook.find_by(id: params[:webbook_id])
     redirect_to root_path unless user_has_book?
 
     @page = Page.where(webbook_id: params[:webbook_id]).find(params[:id])
@@ -34,5 +35,12 @@ class PagesController < ApplicationController
   def page_params
     params.require(:page)
           .permit(:title, :content)
+  end
+
+  def user_has_book?
+    current_user.purchase_history
+      .purchase_history_webbooks
+      .find_by(webbook_id: params[:webbook_id])
+      .present?
   end
 end
