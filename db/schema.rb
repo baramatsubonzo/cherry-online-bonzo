c3af8ec694402353d36429bcf753e86f4eb932a0
baramatsubonzo <baramatsubonzo@gmail.com>
2020-10-23 15:54:29 +0900

    Webbookモデルのreleaseのデフォルト値falseを追加した

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_22_104932) do
+ActiveRecord::Schema.define(version: 2020_10_23_062801) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -93,7 +93,7 @@ ActiveRecord::Schema.define(version: 2020_10_22_104932) do
     t.text "description"
     t.integer "price"
     t.date "release_date"
-    t.boolean "release"
+    t.boolean "release", default: false, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
