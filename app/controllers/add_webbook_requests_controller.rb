b4ae5df1dab6c3cebd227ed26e24f50461f06799
baramatsubonzo <baramatsubonzo@gmail.com>
2020-10-26 16:44:46 +0900

    カートにある商品は購入できないようにした

--- app/controllers/add_webbook_requests_controller.rb
+++ app/controllers/add_webbook_requests_controller.rb
@@ -1,8 +1,8 @@
 class AddWebbookRequestsController < ApplicationController
+  before_action :prevent_add_cart, only:[:create]
+
   def create
-    if @cart_webbooks.blank?
-      @cart_webbook = current_user.cart.cart_webbooks.build(webbook_id: params[:webbook_id])
-    end
+    @cart_webbook = current_user.cart.cart_webbooks.build(webbook_id: params[:webbook_id]) if @cart_webbooks.blank?
 
     @cart_webbook.save
 
@@ -13,5 +13,16 @@ class AddWebbookRequestsController < ApplicationController
     cart_webbook = current_user.cart.cart_webbooks.find_by(webbook_id: params[:webbook_id])
     cart_webbook.destroy
     redirect_to current_cart
+  end
+
+  def prevent_add_cart
+    redirect_to root_path, notice: "すでにカートにある商品です" if cart_has_book?
+  end
+
+  def cart_has_book?
+    current_user.cart
+      .cart_webbooks
+      .find_by(webbook_id: params[:webbook_id])
+      .present?
   end
 end
