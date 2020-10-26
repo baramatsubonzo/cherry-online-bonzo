fa09df90a8d3baf8ab322971accdaab895eda203
baramatsubonzo <baramatsubonzo@gmail.com>
2020-10-26 14:19:24 +0900

    管理者画面から購入画面への導線確保と、管理者のみ購入履歴を閲覧できるように制限

--- app/controllers/admin/purchase_histories_controller.rb
+++ app/controllers/admin/purchase_histories_controller.rb
@@ -1,5 +1,13 @@
 class Admin::PurchaseHistoriesController < ApplicationController
+  before_action :require_admin
+  
   def index
     @purchase_history_webbooks = PurchaseHistoryWebbook.all
+  end
+
+  private
+
+  def require_admin
+    redirect_to root_path unless current_user.admin?
   end
 end
