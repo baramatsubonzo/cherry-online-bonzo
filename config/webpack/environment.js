f7e4785ac7243dc555b5218b636873d2a77ea7c5
baramatsubonzo <baramatsubonzo@gmail.com>
2020-10-27 17:03:10 +0900

    jqeury-uiを導入した

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -6,9 +6,18 @@ const webpack = require('webpack')
 environment.plugins.prepend('Provide',
     new webpack.ProvidePlugin({
         $: 'jquery/src/jquery',
-        jQuery: 'jquery/src/jquery'
+        jQuery: 'jquery/src/jquery',
+        Popper: ['popper.js', 'default']
     })
 )
+
+environment.toWebpackConfig().merge({
+	resolve: {
+    alias: {
+      'jquery': 'jquery/src/jquery'
+    }
+  }
+})
 
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
