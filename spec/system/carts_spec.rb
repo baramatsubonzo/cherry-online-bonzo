7fe40d2a6184b460fedb1497b8656493d517f5c1
baramatsubonzo <baramatsubonzo@gmail.com>
2020-10-29 18:21:45 +0900

    カートから削除する機能のテスト

--- spec/system/carts_spec.rb
+++ spec/system/carts_spec.rb
@@ -44,9 +44,24 @@ RSpec.describe "カート", type: :system do
   end
 
   describe 'エンドユーザーはカートからWebブックを削除できる' do
-    
+    let(:user_a) { FactoryBot.create(:user) }
+    let(:webbook_a) { FactoryBot.create(:webbook) }
+    let(:cart) { FactoryBot.create(:cart, user: user_a) }
+    let!(:cart_webbook) { FactoryBot.create(:cart_webbook, cart: cart, webbook: webbook_a) }
+
+    before do
+      visit login_path
+      fill_in 'メールアドレス', with: user_a.email
+      fill_in 'パスワード', with: user_a.password
+      click_button 'ログインする'
+
+      visit cart_path(user_a.cart.id)
+      click_link '削除'
+    end
     it 'カートから商品が削除される' do
-    
+      expect(page.driver.browser.switch_to.alert.text).to eq "カートから削除してよろしいですか？"
+      page.driver.browser.switch_to.alert.accept
+      expect(page).to have_current_path cart_path(user_a.cart.id)
     end
   end
 end
