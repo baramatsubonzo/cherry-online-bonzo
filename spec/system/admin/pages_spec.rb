09b4143070a5524f39941b07b07806f36de98b06
baramatsubonzo <baramatsubonzo@gmail.com>
2020-10-30 17:13:46 +0900

    page作成機能のテスト

--- spec/system/admin/pages_spec.rb
+++ spec/system/admin/pages_spec.rb
@@ -3,25 +3,64 @@ require 'rails_helper'
 RSpec.describe "Admin#Page", type: :system do
   describe 'page#new' do
     describe '管理者の場合page#newが表示される' do
-      context '管理者の場合' do
-        it 'page作成ページが表示される' do
+    let(:webbook_a){FactoryBot.create(:webbook)}
+      context '管理者の場合' do        
+        before do
+          FactoryBot.create(:admin_user)
+          visit login_path
+          fill_in 'メールアドレス', with: 'admin@sample.com'
+          fill_in 'パスワード', with: 'password'
+          click_button 'ログインする'
+  
+          visit new_admin_webbook_path
         end
+        it 'page作成ページが表示される' do
+          expect(page).to have_current_path new_admin_webbook_path
+        end
+
       end
 
       context 'ユーザーの場合' do
+        before do
+          FactoryBot.create(:user)
+          visit login_path
+          fill_in 'メールアドレス', with: 'user_1@sample.com'
+          fill_in 'パスワード', with: 'password'
+          click_button 'ログインする'
+  
+          visit new_admin_webbook_page_path(webbook_a.id)
+        end
         it 'トップページへリダイレクトされる' do
+          expect(page).to have_current_path root_path
         end
       end
     end
 
     describe 'pageを作成することができる' do
+      let(:webbook_a){FactoryBot.create(:webbook)}
       context 'pageが追加される' do
-        it 'pageのレコードが追加される' do
+        before do
+          FactoryBot.create(:admin_user)
+          FactoryBot.create(:page, webbook_id: webbook_a.id)
+          visit login_path
+          fill_in 'メールアドレス', with: 'admin@sample.com'
+          fill_in 'パスワード', with: 'password'
+          click_button 'ログインする'
+  
+          visit new_admin_webbook_page_path(webbook_a.id)
+          fill_in 'Title', with: 'テストタイトル'
+          fill_in 'Content', with: '最高のコンテンツ'
+          click_button '送信'
+        end
+
+        it 'Pageのレコードが追加される' do
+          # RFC: レコードが一つ追加されると書いたが、これでいいか？
+          expect(Webbook.find_by(id: webbook_a).pages.count).to eq 2
         end
       end
 
-      context 'pageが追加されない' do
-      end
+      # context 'pageが追加されない' do
+      # end
     end
   end
 
