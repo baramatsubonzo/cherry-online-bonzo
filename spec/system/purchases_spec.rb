c2745ef93ddc1090b75351699be564e3be17f6a9
baramatsubonzo <baramatsubonzo@gmail.com>
2020-11-02 10:05:21 +0900

    発売日が今日より後の場合購入できないテストの実装

--- spec/system/purchases_spec.rb
+++ spec/system/purchases_spec.rb
@@ -81,10 +81,38 @@ RSpec.describe "Webbookを購入する", type: :system do
   end
 
   describe '発売日がまだ来ていないWebブックは購入できない' do
-    context '発売日が今日より後の場合(購入できない)' do
-    end
+    let(:user_a) { FactoryBot.create(:user) }
+    let(:webbook_a) { FactoryBot.create(:webbook, release: false) }
+    let(:webbook_b) { FactoryBot.create(:webbook, title: '履歴にある本') }
+    let(:cart) { FactoryBot.create(:cart, user: user_a) }
+    let!(:cart_webbook) { FactoryBot.create(:cart_webbook, cart: cart, webbook: webbook_a) }
+    let(:purchase_history) { FactoryBot.create(:purchase_history, user: user_a) }
+    let!(:purchase_history_webbook) { FactoryBot.create(:purchase_history_webbook, purchase_history: purchase_history, webbook: webbook_b) }
 
-    context '発売日が今日より前の場合(購入できる)' do
+    context '発売日が今日より後の場合(購入できない)' do
+      before do
+        FactoryBot.create(:page, webbook_id: webbook_a.id)
+        visit login_path
+        fill_in 'メールアドレス', with: 'user_1@sample.com'
+        fill_in 'パスワード', with: 'password'
+        click_button 'ログインする'
+
+        visit cart_path(user_a.cart.id)
+        click_button '購入する'
+        sleep(2)
+
+        Capybara.within_frame 'stripe_checkout_app' do
+            fill_in "カード番号", :with => "4242424242424242"
+            fill_in '有効期限', :with => '11/22'
+            fill_in 'CVC', :with => '123'
+
+            click_button "を支払う"
+          sleep(3) 
+        end
+      end
+      it '商品が購入できず、アラートが表示されている' do
+        expect(page).to have_content '発売日前の商品です'
+      end
     end
   end
 end
